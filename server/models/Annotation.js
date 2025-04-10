97e561e861e410514d3d657cd5ab7dcb724e4da8
kornvayu <145351543+kunza10153@users.noreply.github.com>
2025-04-10 20:57:46 +0700

    test22

--- server/models/Annotation.js
+++ server/models/Annotation.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
 
-const annotationSchema = new mongoose.Schema({submission_id: {type: mongoose.Schema.Types.ObjectId,ref: "Submission",required: true,},
+const annotationSchema = new mongoose.Schema({
+  submission_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Submission",
+    required: true,
+  },
   file_url: { type: String, required: true },
   page_number: { type: Number, required: true },
   highlight_text: { type: String, required: true },
   bounding_box: { type: Object, required: true },
-  professor_id: {type: mongoose.Schema.Types.ObjectId,ref: "User",required: true,},
+  professor_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   highlight_color: { type: String, default: "#ffeb3b" },
-  comment: { type: String, required: true },
+  comment: { type: String, required: false },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }], // เก็บรายการ Comment
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
