0e590cbc4bba56c1084acc5430037013a8edb558
LAKPET <sakulromyen@hotmail.com>
2025-05-05 22:19:30 +0700

    add uniuq course,section

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
-  course_number: { type: String, required: true },
+  course_number: { type: String, required: true, unique: true },
   course_name: { type: String, required: true },
   course_description: { type: String },
   sections: [{ type: mongoose.Schema.Types.ObjectId, ref: "Section" }]  // เชื่อมโยง Section ที่เกี่ยวข้อง
