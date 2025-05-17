9d17cde307aa841886c4df7e74d9eaf10337f0b5
kornvayu <145351543+kunza10153@users.noreply.github.com>
2025-05-17 16:42:40 +0700

    --

--- server/server.js
+++ server/server.js
@@ -27,6 +27,23 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
 
+app.get("/pdf", async (req, res) => {
+  const fileUrl = req.query.url;
+  try {
+    const response = await fetch(fileUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch PDF: ${response.statusText}`);
+    }
+    const buffer = await response.arrayBuffer();
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(Buffer.from(buffer));
+  } catch (error) {
+    console.error("Error fetching PDF:", error);
+    res.status(500).send("Error fetching PDF");
+  }
+});
+
 // ใช้ตัวแปร DB_link จาก .env
 const mongoUri = process.env.DB_link;
 
