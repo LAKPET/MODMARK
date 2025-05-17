40d8cf03664e42e75a5e2493d4aa600a4a3a0bd4
LAKPET <sakulromyen@hotmail.com>
2025-05-17 21:32:33 +0700

    ..

--- server/services/firebaseConfig.js
+++ server/services/firebaseConfig.js
@@ -1,10 +1,14 @@
 const admin = require("firebase-admin");
 
 // ใช้ path ตรงไปยังไฟล์ Service Account Key
-const serviceAccount = require(process.env.FIREBASE_PATH);
+const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // แปลง \n กลับเป็น newline จริง
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Set this in your .env file
 });
 
